d704d42e340838d52f3e30fe3a083b6e47893e13
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-16 23:57:54 +0300

    Update 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main -Entry poit
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int f, s;
+
+    s='0';
+	for (f = 48; f < 57 ; f++)
+	{    s=f+1;
+	    while (s < 58)
+            {
+            putchar(f);
+            putchar(s);
+            s++;
+            if(f!=56){
+            putchar(',');
+            putchar(' ');
+            }
+	    }
+
+	}
+	putchar('\n');
+	return (0);
+}
