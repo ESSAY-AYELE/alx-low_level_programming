d1b8e2e0f2143a9a619fdd61af912145add13e9e
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-16 11:36:40 -0700

    third task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main -Entry poit
+ *
+ *   *
+ *
+ *    *Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+	char c;
+
+	c ='a';
+	for(c;c<'z'+1;c++)
+	{
+		putchar(c);
+		putchar('\n');
+
+	}
+     	return (0);
+
+}
