d5653795e22083224f5f2524cdc7da299d9be22e
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-16 22:31:19 +0300

    Update 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -Entry poit
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c < 'z' + 1 ; c++)
+	{
+		putchar(c);
+    if (c == 'd' || c=='p')
+        c++;
+	}
+	putchar('\n');
+	return (0);
+}
