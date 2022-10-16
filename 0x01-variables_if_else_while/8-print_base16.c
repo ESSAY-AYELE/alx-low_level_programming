323591a46a6e25208986586c349cb961eded9752
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-16 23:13:13 +0300

    Update 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -Entry poit
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c < 'g' ; c++)
+	{
+		putchar(c);
+    if (c == 57)
+    c=='a'-1
+	}
+	putchar('\n');
+	return (0);
+}
