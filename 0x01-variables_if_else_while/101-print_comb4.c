45eca864c5dc25dc37061b372d01f9be3a361004
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-17 00:11:19 +0300

    Update 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main -Entry poit
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int f, s, t;
+
+    s='0';
+	for (f = 48; f < 57 ; f++)
+	{    s=f+1;
+
+	    while (s < 57)
+            {
+
+                t=s+1;
+                while(t<58){
+            putchar(f);
+            putchar(s);
+            putchar(t);
+            t++;
+            if(f!=55){
+            putchar(',');
+            putchar(' ');
+            }}s++;
+	    }
+	}
+	putchar('\n');
+	return (0);
+}
