20cdfe963d484ebe2d19c63c6b60ca79c83a1801
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-17 02:27:26 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,21 +7,21 @@
 int main(void)
 {
 	int f, s;
-
-    s='0';
+	
+	s = '0';
 	for (f = 48; f < 57 ; f++)
-	{    s=f+1;
-	    while (s < 58)
-            {
-            putchar(f);
-            putchar(s);
-            s++;
-            if(f!=56){
-            putchar(',');
-            putchar(' ');
-            }
-	    }
-
+	{    s = f + 1;
+	 while (s < 58)
+	 {
+		 putchar(f);
+		 putchar(s);
+		 s++;
+		 if (f!=56)
+		 {
+			 putchar(',');
+			 putchar(' ');
+		 }
+	 }
 	}
 	putchar('\n');
 	return (0);
