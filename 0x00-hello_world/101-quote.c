f0e3f1b4841897d05713dba718a58b056fc9d6dc
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-17 23:34:23 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 /**
- *
- *  * main -Entry poit
- *
- *   *
- *
- *    *Return: Always 0 (Success)
- *
- *     */
-
+*
+*  * main -Entry poit
+*
+*   *
+*
+*    *Return: Always 0 (Success)
+*
+*/
 int main(void)
-
 {
 	int i, j;
 
 	char word[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	j=strlen(word);
+
+	j = strlen (word);
 	for(i = 0; i < j; i++)
 		putchar(word[i]);
-	return (0);
+	putchar('\n');
+	return (1);
 
 }
-
-
