7f5c140ea433c56ace5215d987de7425138c11a9
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-17 23:36:36 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 /**
-*
-*  * main -Entry poit
+*main -Entry poit
 *
 *   *
 *
-*    *Return: Always 0 (Success)
+*Return: Always 0 (Success)
 *
 */
 int main(void)
@@ -15,8 +14,8 @@ int main(void)
 
 	char word[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	j = strlen (word);
-	for(i = 0; i < j; i++)
+	j = strlen(word);
+	for (i = 0; i < j; i++)
 		putchar(word[i]);
 	putchar('\n');
 	return (1);
