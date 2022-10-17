8d149e1f444d978527fbc9cdb9aa877493ec66cb
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-17 03:12:24 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,31 +6,33 @@
  */
 int main(void)
 {
-	char f, s,t ,l;
+	char f, s, t, l;
 
-	for (f = '0'; f < '9'+1 ; f++)
-    {
-        for (s = '0'; s < '9'+1 ; s++)
-        {
-            for (t = f; t < '9'+1 ; t++)
-            {
+	for (f = '0'; f < '9' + 1 ; f++)
+	{
+		for (s = '0'; s < '9' + 1 ; s++)
+		{
+			for (t = f; t < '9' + 1 ; t++)
+			{
+				for (l = s + 1; l < '9' + 1 ; l++)
+				{
+					putchar(f);
+					putchar(s);
+					putchar(' ');
+					putchar(t);
+					putchar(l);
+					if (!(f == '9' && s == '8' && t == '9' && l == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
 
-                for (l = s + 1; l < '9'+1 ; l++)
-                {
-                    putchar(f);
-                    putchar(s);
-                    putchar(' ');
-                    putchar(t);
-                    putchar(l);
-                    putchar(',');
-                    putchar(' ');
+				}
+			}
 
-                }
-            }
+		    }
 
-            }
-
-        }
+	}
 	putchar('\n');
 	return (0);
 }
