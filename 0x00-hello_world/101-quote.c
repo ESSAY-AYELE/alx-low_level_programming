13e1dd05c1d4114c9ce50766eb38128bef4a26cb
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-17 13:29:53 -0700

    final advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *
+ *  * main -Entry poit
+ *
+ *   *
+ *
+ *    *Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+	int i, j;
+
+	char word[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	j=strlen(word);
+	for(i = 0; i < j; i++)
+		putchar(word[i]);
+	return (0);
+
+}
+
+
