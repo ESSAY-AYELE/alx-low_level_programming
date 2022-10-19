646a657a4585bd7719c7b21afb9517aaf211c2a8
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-19 23:07:33 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,15 @@
 #include <ctype.h>
 /**
-*
-*_islower - check whether lower case or not.
-*   *
-*
-*Return: 1 if c if lowercase else 0.
-*
-*    
+ *
+ *_islower - check whether lower case or not.
+ *   *
+ *
+ *Return: 1 if c if lowercase else 0.
+ *    
 */
-
 int _islower(int c)
 {
 	if (islower(c) != 0)
 		return (1);
 	else return (0);
 }
-
