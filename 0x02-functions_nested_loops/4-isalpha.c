776fd87891630354ad7d34a13b4632cec7cc3bf7
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-19 15:43:23 +0300

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include <ctype.h>
+/**
+*_islower - check wethere lower or not.
+*
+* Return: 0 if it is not lowwer else random int.
+*/
+int _isalpha(int c)
+{
+    if (isalpha(c) != 0)
+        return (1);
+    else return (0);
+}
