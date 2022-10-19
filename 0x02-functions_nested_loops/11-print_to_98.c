f0508970d55b3200b60a6149d70d415a3d819fac
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-19 18:37:22 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,20 @@
+#include "main.h"
 void print_to_98(int n)
 {
+  if (n > 98)
+   greater(n);
+  else if (n > 0 && n < 98)
+    less(n);
+  else
+    negative(n);
+}
+void greater(int n){
   int i;
- for(i = n; n < 99; i++)
- {
-    if (i<
-    _putchar
+  for (i = n; i > 97; i--){
+    _putchar('0' + i);
+    if (i!=98){
+    _putchar(',');
+    }_putchar(' ');
+    
+
+  
