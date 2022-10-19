b4c5542c81b9b657150c32428cec9fde550cb56f
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-19 12:17:59 +0300

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "_putchar.h"
+#include "1-main.h"
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return: Always void.
+ */
+void print_alphabet(void)
+{
+  char i;
+  
+  for (i= 97; i < 123; i++)
+    _putchar(i);
+  _putchar(i);
+}
