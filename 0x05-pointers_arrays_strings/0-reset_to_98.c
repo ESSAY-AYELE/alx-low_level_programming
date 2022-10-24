7d178f3829523d0fc2a07537bb61b5a46e54a571
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-24 22:04:50 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 /**
  * reset_to_98 - change the value of the variabel that was send to 403
- * n: holds varibles address
- * Return: void
+ *
+ * @n: holds varibles address
+ *
  */
 
 void reset_to_98(int *n)
