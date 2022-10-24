f9198d6c4f9decee857d30e027d32014619e87f9
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-25 02:10:09 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,10 +4,11 @@
  * _strcpy - copy one string to onther arry
  * @dest: copied to
  * @src: copied from
- *
+ * 
+ *Return: the dest array
  */
 char *_strcpy(char *dest, char *src)
 {
-	strcpy(dest,src);
+	strcpy(dest, src);
 	return (dest);
 }
