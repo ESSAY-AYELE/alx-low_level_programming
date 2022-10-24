13f4a1a5ce80cfd8b4d662e676242a3b69d54153
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-24 13:09:40 -0700

    third task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <string.h>
+/**
+ * _strlen - calculate the length of string
+ * @s: the pointer of the string
+ * Return: lenght of the string
+ *
+ */
+int _strlen(char *s)
+{
+	int i = strlen(s);
+
+	return i;
+}
