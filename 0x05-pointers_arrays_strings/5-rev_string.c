05d6babd5489d63051fa0f9b267f8391a2d866aa
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-25 00:40:59 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,6 +8,7 @@
 void rev_string(char *s)
 {
 	int i, len;
+
 	char temp;
 	
 	len = strlen(s);
@@ -15,5 +16,6 @@ void rev_string(char *s)
 	{
 		temp = s[i];
 		s[i] = s[len - 1 - i];
+		s[len - 1 -i] = temp;
 	}
 }
