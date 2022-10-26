cd950c492df1cf812e7385fd260cd69e4bcbac6c
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-26 01:53:31 -0700

    first task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include <string.h>
+/**
+ * _strcat - concatenates to strings
+ * @dest: the fitst string
+ * @src: the second string
+ * Return: the mereged string
+ *
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest,src));
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_FILE
+#define HEADRR_FILE
+
+int _putchar(char);
+
+char *_strcat(char *dest, char *src);
+
+#endif
