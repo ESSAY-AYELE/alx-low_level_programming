2a96ed038521841e68dc57878c2f47edc0e64c24
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-27 08:44:03 -0700

    third task

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,12 @@
 #include <string.h>
 /**
- * _strcat - concatenates to strings
+ * _strcpy - concatenates to strings
  * @dest: the fitst string
  * @src: the second string
  * @n: the number of bytes
  * returns: ponter to src
  */
-char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 {
-	return (strncat(dest, src, n));
+	return (strncpy(dest, src, n));
 }
