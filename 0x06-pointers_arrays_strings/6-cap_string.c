c9865876afee2bbf2a56cb33f263e75ced2e2642
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-28 01:41:25 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,16 +8,18 @@
 char *cap_string(char *str)
 {
 	int i, len, j, len2;
-	char c =" .,{}()!?;\t\n\""
+	char c[] =" .,{}()!?;\t\n\""
 
 	len = strlen(str);
-	len = strlen(c);
+	len2 = strlen(c);
 	str[0] = toupper(str[0]);
 	for (i = 0; i < len - 1; i++)
 	{
 		for (j = 0; j < len2; j++)
+		{
 			if (str[i] == c[j])
 				str[i + 1] = toupper(str[i + 1]);
+		}
 	}
 	return (str);
 }
