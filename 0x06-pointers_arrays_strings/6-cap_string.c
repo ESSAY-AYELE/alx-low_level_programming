50e194f60e2343a99ddc29307712eae9a23d8f52
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-27 13:23:31 -0700

    seventh task

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <ctype.h>
+/**
+ * cap_string - capitalizes string
+ * @str: the string
+ * Return: the result
+ */
+char *cap_string(char *str)
+{
+	int i, len;
+
+	len = strlen(str);
+	for (i = 0; i < len - 1; i++)
+	{
+		if (str[i] == ' ' || str[i] == '.' || str[i] == ',')
+			str[i + 1] = toupper(str[i + 1]);
+	}
+	return (str);
+}
