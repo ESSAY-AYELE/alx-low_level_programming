de7aa7a707a7a482475f2abbf76d28c171d56b77
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-28 01:36:06 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,16 +7,17 @@
  */
 char *cap_string(char *str)
 {
-	int i, len;
+	int i, len, j, len2;
+	char c =" .,{}()!?;\t\n\""
 
 	len = strlen(str);
+	len = strlen(c);
 	str[0] = toupper(str[0]);
 	for (i = 0; i < len - 1; i++)
 	{
-		if (str[i] == ' ' || str[i] == '.' || str[i] == ',' || str[i] == ';' || str[i] == '!' ||
-			str[i] == '?' || str[i] == '(' || str[i] == ')' || str[i] == '\n' 
-				|| str[i] == '\t' || str[i] == '{')
-			str[i + 1] = toupper(str[i + 1]);
+		for (j = 0; j < len2; j++)
+			if (str[i] == c[j])
+				str[i + 1] = toupper(str[i + 1]);
 	}
 	return (str);
 }
