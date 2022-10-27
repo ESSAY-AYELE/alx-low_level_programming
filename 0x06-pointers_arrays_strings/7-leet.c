8368c14085b443fe15f4a426bf2c79ca6bc29040
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-28 01:14:19 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,16 +7,16 @@
 char *leet(char *str)
 {
 	int i, j, len;
-	char lower[] = ['a','e','l','o','s'];
-	char upper[] = ['A','E','L','O','S'];
-	char leet[] = ['4','3','1','0','7'];
+	char lower[] = "aelot";
+	char upper[] = "AELOT";
+	char leet[] = "43107";
 
 	len = strlen(str);
 	for (i = 0; i < len; i++)
 	{
 		for (j = 0; j < 5; j++)
 			if (str[i] == lower[j] || str[i] == upper[j])
-				str[i] = leet [j];
+				str[i] = leet[j];
 	}
 	return (str);
 }
