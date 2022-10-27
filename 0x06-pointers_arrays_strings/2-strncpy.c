7c3b4bf43ffedbc450aa23e76ccda272b6ef37ec
ESSAY-AYELE <misganaa352@gmail.com>
2022-10-27 18:52:00 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,7 +4,7 @@
  * @dest: the fitst string
  * @src: the second string
  * @n: the number of bytes
- * returns: ponter to src
+ * Returns: ponter to src
  */
 char *_strncpy(char *dest, char *src, int n)
 {
