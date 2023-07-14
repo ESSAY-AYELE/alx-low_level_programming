189752bb9f0666a81d55a1f49b114c457ca63db7
ESSAY-AYELE <misganaa352@gmial.com>
2023-07-14 13:37:56 -0700

    102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			l = j + 1;
+			for (k = i; k <= '9';k++)
+			{
+				while (l <= '9')
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (!((i == '9') && (j == '8') && (k == '9') && (l == '9')))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					l++;
+				}	
+				l = '0';
+			}
+		}
+	}
+	putchar('\n');		
+	return (0);
+}
