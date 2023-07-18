7f4dcb1eefd8f4ec083550386de2c900d2fd2379
ESSAY-AYELE <misganaa352@gmial.com>
2023-07-18 09:32:52 -0700

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * times_table - prints time table from 0 to 9
+ * Return: void
+ */
+void times_table(void)
+{
+	int i, j, pro, lastDigit, firstDigit;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			pro = i * j;
+			lastDigit = pro % 10;
+			firstDigit = (pro - lastDigit) / 10;
+			if (pro >= 10)
+				_putchar('0' + firstDigit);
+			_putchar(lastDigit);
+			if (!((i == 9) && (j == 9)))
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
