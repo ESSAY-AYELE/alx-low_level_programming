1dee92aa956d3349dad2dadea6da5128dae4c8d4
ESSAY-AYELE <misganaa352@gmail.com>
2023-07-18 20:53:38 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * print_to_98 - print up to 98 from any given integer
+ * @n: the starting number
+ * Return: void
+ */
 void print_to_98(int n)
 {
 	if (n == 98)
@@ -7,7 +12,7 @@ void print_to_98(int n)
 		printf("%d\n", n);
 		return;
 	}
-	do{
+	do {
 		printf("%d, ", n);
 		if (n < 98)
 			n++;
