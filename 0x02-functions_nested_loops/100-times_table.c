2a7848b6ef58489b18b128aec3698a79b3bb8122
ESSAY-AYELE <misganaa352@gmail.com>
2023-07-18 21:42:39 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,6 +8,8 @@ void print_times_table(int n)
 {
 	int i, j, pro, lastDigit, firstDigit, secondDigit;
 
+	if ((n > 15) || (n < 0))
+		return;
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
