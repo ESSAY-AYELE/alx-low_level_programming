3f76ad13a8b543ed5ade210d5794b6d74c78bfe8
ESSAY-AYELE <misganaa352@gmial.com>
2023-07-18 05:43:12 -0700

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - check if a char is lower case or not
+ * @c: the character be checked
+ * Return: 1 if is lowercase 0 if not
+ */
+int _isalpha(int c)
+{
+	if i(((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+		return (1);
+	else
+		return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,8 +3,14 @@
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-<<<<<<< HEAD
-int _islower(int c);
-=======
->>>>>>> a5848958306637d6cbe7430c6ec5de3783342e7a
+int _islower(int);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 #endif
