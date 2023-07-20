264878948e703beca1c173142884bacb4b3c07c0
ESSAY-AYELE <misganaa352@gmial.com>
2023-07-20 06:15:27 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-prints_line.c
@@ -0,0 +1,14 @@
+#include "main"
+/**
+ * print_line - print a line
+ * n: number of - is printed
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	putchar('\n');
+}
