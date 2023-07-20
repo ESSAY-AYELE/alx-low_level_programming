a2dfd8b6e20242f241f5ecc3171dada57ca4cc56
ESSAY-AYELE <misganaa352@gmial.com>
2023-07-20 12:24:37 -0700

    10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangle
+ * @size: the size of triangle
+ */
+void print_triangle(int size)
+{
+	int i, j, k, l;
+
+	for (i = 1; i <= size; i++)
+	{
+		l = size - i; 
+		for (j = 0; j < l; j++)
+			_putchar(' ');
+		for (k = 0; k < i; k++)
+			_putchar('#');
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
