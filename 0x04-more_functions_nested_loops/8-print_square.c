09b770e0f234ca57ac7082ae1425cc1efda8e46f
ESSAY-AYELE <misganaa352@gmial.com>
2023-07-20 11:08:17 -0700

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#inlcude "main.h"
+/**
+ * print_square - prints square from #
+ * @size: the size of square
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (n <= 0)
+		_putchar('\n');
+}
