8f6ae03b11506c499f0718185347bd2221f11516
ESSAY-AYELE <misganaa352@gmial.com>
2023-07-25 12:53:17 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * rev_string - reversers a stirng
+ * @s: the string pointer
+ */
+void puts_half(char *str)
+{
+	int i, j, k;
+	char tmp = 'A';
+
+	for (i = 0; tmp != '\0'; i++)
+		tmp = s[i];
+	i -= 2;
+	k = i / 2;
+	for (j = k; j <= i; j++)
+		_putchar(str[j]);
+}
