b7726bad3b3cd68ea013d7d7f28ae35c6f44b747
ESSAY-AYELE <misganaa352@gmail.com>
2023-07-25 19:03:09 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,18 +4,18 @@
  * @s: the pointer to the stirng
  */
 void print_rev(char *s)
-{	int i, j;
+{	
+	int i, j;
 	char tmp;
 
 	for (i = 0; tmp != '\0'; i++)
 	{
 		tmp = *(s + i);
 	}
- 	s = s + i - 1;
+	s = s + i - 1;
 	for (j = 0; j < i; j++)
 	{
 		_putchar(*(s - j));
 	}
 	_putchar('\n');
-			
 }
