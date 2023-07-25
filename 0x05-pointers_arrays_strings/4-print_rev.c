17ca638bd381e4ff4c6dec22ac4da419d56de263
ESSAY-AYELE <misganaa352@gmail.com>
2023-07-25 18:36:18 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,8 +8,13 @@ void print_rev(char *s)
 	char tmp;
 
 	for (i = 0; tmp != '\0'; i++)
+	{
 		tmp = *(s + i);
+	}
 	for (j = i - 1; j >= 0; j--)
+	{
 		_putchar(*(s + j));
+	}
 	_putchar('\n');
+			
 }
