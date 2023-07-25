9710aa24a1e892d08cdbaf833a550231c2c7a6d8
ESSAY-AYELE <misganaa352@gmial.com>
2023-07-25 12:16:56 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - reversers a stirng
+ * @s: the string pointer
+ */
+void rev_string(char *s)
+{
+	int i, j, k;
+	char tmp = 'A';
+
+	for (i = 0; tmp != '\0'; i++)
+		tmp = *(s + i);
+	i -= 1;
+	k = i / 2;
+	for (j = 0; j <= k; j++)
+	{
+		tmp = s[j];
+		s[j] = s[i];
+		s[i] = tmp;
+		i--;
+	}
+}
