f5287e4023600fca8fe43287b9ce33162a1b2de5
ESSAY-AYELE <misganaa352@gmail.com>
2023-07-26 21:55:24 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,14 @@ char *_strcat(char *dest, char *src)
 	int i, j;
 
 	j = 0;
+	i = 0;
 	while (dest[i] != '\0')
 	{
 		dest[j] = dest[i];
 		i++;
 		j++;
 	}
-	
+	i = 0;
 	while (src[i] != '\0')
 	{
 		dest[j] = src[i];
