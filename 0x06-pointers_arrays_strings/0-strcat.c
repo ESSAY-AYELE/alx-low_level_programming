f47359d039fdea03dfddded1969e9467e4949494
ESSAY-AYELE <misganaa352@gmial.com>
2023-07-26 11:29:51 -0700

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strcat - concatenate the strings
+ * @src: source
+ * @dest: destination
+ * Return: pointer to the string
+ */
+char *_strcat(char *dest, char *src)
+{
+	dest = dest + src;
+	return dest;
+}
