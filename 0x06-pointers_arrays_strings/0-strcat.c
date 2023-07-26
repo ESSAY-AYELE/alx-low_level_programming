2083865f646465d8215ef73b36cc12ffdcd60293
ESSAY-AYELE <misganaa352@gmial.com>
2023-07-26 11:48:42 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,6 +7,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	dest = dest + src;
+	int i, j;
+
+	j = 0;
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		dest[j] = dest[i];
+		j++;
+	}
+	j++;
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[j] = src[i];
+		j++;
+	}
+	dest[j + 1] = '\0';
 	return dest;
 }
