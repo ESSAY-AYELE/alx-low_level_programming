c5aff117eb6790aab1486a86b85314e89eab72af
ESSAY-AYELE <misganaa352@gmail.com>
2023-07-27 20:56:13 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,17 +1,25 @@
 #include "main.h"
 /**
  * _strcmp - compares the length of 2 string
- * @s1: the first stirng pointer
+ * @s1: the first string pointer
  * @s2: the second string pointer
- * Return: +ve int if s2 < s1 else -ve int 0,if they are equll
+ * Return: +ve int if s2 < s1 else -ve int 0, if they are equal
  */
 int _strcmp(char *s1, char *s2)
 {
-	unsigned int i = 0, j = 0;
+	int i = 0;
 
-	while (s1[i] != '\0')
+	while ((s1[i] != '\0') && (s2[i] != '\0'))
+	{
+		if (s1[i] > s2[i])
+			return (1);
+		else if (s1[i] < s2[i])
+			return (-1);
 		i++;
-	while (s2[j] != '\0')
-		j++;
-	return (i - j);
+	}
+	if ((s1[i] == '\0') && (s2 != '\0'))
+		return (-1);
+	else if ((s1[i] != '\0') && (s2[i] == '\0'))
+		return (1);
+	return (0);
 }
