f48b4942e6757c7780e3cb89c72e0667617911de
ESSAY-AYELE <misganaa352@gmial.com>
2023-07-27 12:54:18 -0700

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * leet - encode a string in 1337 format
+ * @s: the string pointer
+ * Return: the string
+ */
+char *leet(char *s)
+{
+	int i, num;
+	char c;
+
+	for (i = 0; s[i]; i++)
+	{
+		c = s[i];
+		num = (c == 'A' || c == 'a') * 4 + (c == 'e' || c == 'E') * 3 + \
+		      (c == 'O' || c == 'o') * 0 + (c == 't' || c == 'T') * 7 + (c == 'l' || c == 'L') * 1;
+		if (num)
+			s[i] = '0' + num;
+	}
+	return (s);
+}
