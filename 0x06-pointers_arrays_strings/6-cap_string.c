fe9b417369a0a21ad2ad62df7e6742460cf871cd
ESSAY-AYELE <misganaa352@gmial.com>
2023-07-27 13:50:34 -0700

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if ((s[i - 1] == ' ' || i == 0 || s[i - 1] == '\n' || s[i - 1] == '\t' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')'|| str[i - 1] == '{' || str[i - 1] == '}') && s[i] >= 'a' && s[i] <= 'z') 
+			s[i] -= 'a' - 'A';
+	}
+	return (s);
+}			
