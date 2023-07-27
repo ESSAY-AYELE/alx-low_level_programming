08818d234228817bb3fdcc56d6f066fdcbc93923
ESSAY-AYELE <misganaa352@gmail.com>
2023-07-27 20:56:57 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,7 +17,7 @@ int _strcmp(char *s1, char *s2)
 			return (-1);
 		i++;
 	}
-	if ((s1[i] == '\0') && (s2 != '\0'))
+	if ((s1[i] == '\0') && (s2[i] != '\0'))
 		return (-1);
 	else if ((s1[i] != '\0') && (s2[i] == '\0'))
 		return (1);
