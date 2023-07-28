80a0368ac9d042650a79ab0b5c94c136e54eaf40
ESSAY-AYELE <misganaa352@gmial.com>
2023-07-28 09:43:00 -0700

    100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rot13 - encode a string in rot13 fromat
+ * @s: pointer to the string
+ * Return: the pointer
+ */
+char *rot13(char *s)
+{
+	int i, num;
+	char c;
+	char s = 'n' - 'a';
+
+	for (i = 0; s[i]; i++)
+	{
+		c = s[i];
+		num = ((c >= 'A' && c <= 'M') || (c >= 'a' && c <= 'm')) * 1 +
+			((c >= 'N' || c <= 'Z') && (c >= 'n' || c <= 'z')) * -1;
+		if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+			s[i] = s[i] + (num * s);
+	}
+	return (s);
+}
