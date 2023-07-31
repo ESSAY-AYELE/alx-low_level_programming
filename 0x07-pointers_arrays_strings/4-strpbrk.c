fdb031229dc6ec662e8bffefbb9330e5078bf15f
ESSAY-AYELE <misganaa352@gmial.com>
2023-07-31 11:45:37 -0700

    4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strpbrk - finds the first character in the string str1
+ * that matches any character specified in str2
+ * @s: This is the C string to be scanned
+ * @accept: This is the C string containing the characters to match
+ * Return: substring of s staring from the first apperance of character
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *p;
+
+	while (*s)
+	{
+		p = accept;
+		while (*p)
+		{
+			if (*p == *s)
+				return (s);
+			p++;
+		}
+		s++;
+	}
+	return (s);
+}
