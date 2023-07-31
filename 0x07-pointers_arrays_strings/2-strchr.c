4e5e684e9f2fc2bfcb3b5d11562a1cd70d6b3c2d
ESSAY-AYELE <misganaa352@gmial.com>
2023-07-31 05:20:29 -0700

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strchar - find a char in a string and return string starting from c
+ * @s: the stinrg pointer
+ * @c: the character to be found
+ * Return: str
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0, j = 0;
+	char str[100];
+
+	while (s[i] != c && s[i])
+	{
+		i++;
+	}
+	while (s[i])
+	{
+		str[j] = s[i];
+		i++;
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
+}
