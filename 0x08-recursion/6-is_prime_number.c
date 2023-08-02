42cf30aa9f2139f8baa6564502ce277a1a9a0510
ESSAY-AYELE <misganaa352@gmail.com>
2023-08-03 00:32:50 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * is_pirme_number - check a number is prime or not
+ * is_prime_number - check a number is prime or not
  * @n: the number
  * Return: 1 if it is or  0
  */
@@ -13,7 +13,7 @@ int is_prime_number(int n)
 	return (prime_or_not(n, i));
 }
 /**
- * prime_or _not - check prime or not using recurison
+ * prime_or_not - check prime or not using recurison
  * @n: the number
  * @i: the index
  * Return: 0 or 1
@@ -22,9 +22,8 @@ int prime_or_not(int n, int i)
 {
 	if (i == 1)
 		return (1);
-	else 
+	else
 		if (n % i == 0)
 			return (0);
-		else
-			return (prime_or_not(n, --i));
+	return (prime_or_not(n, --i));
 }
