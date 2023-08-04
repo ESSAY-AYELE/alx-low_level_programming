57875e29d579b72e934e9e7d53da0045935fe85c
ESSAY-AYELE <misganaa352@gmail.com>
2023-08-05 02:01:23 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - prints the name of this program
  * @argc: number of argument
@@ -13,9 +14,9 @@ int main(int argc, char **argv)
 	for (i = 1; i < argc; i++)
 	{
 		j = 0;
-		while (argv[i])
+		while (argv[i][j])
 		{
-			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+			if (!(isdigit(argv[i][j]))
 			{
 				printf("Error\n");
 				return (1);
