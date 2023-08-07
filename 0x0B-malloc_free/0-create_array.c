f649ca660f9fda9e770b618cc719f0feb1a1dd8d
ESSAY-AYELE <misganaa352@gmial.com>
2023-08-07 09:01:52 -0700

    0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * create_array -  create a string array
+ * @size: size of the array
+ * @c: the character to fill it with
+ * Return: pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str = malloc(sizeof(c) * size);
+	int i = 0;
+
+	while (i < size)
+	{
+		str[i] = c;
+		i++;
+	}
+	str[size] = '\0';
+	return (str);
+}
