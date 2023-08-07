c3d0f0c6b8b762063f929106eb1bf591c956f106
ESSAY-AYELE <misganaa352@gmail.com>
2023-08-07 22:17:57 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,14 +7,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *str = malloc(sizeof(c) * size);
+	char str[] = malloc(sizeof(c) * size);
 	unsigned int i = 0;
 
-	str = '\0';
 	while (i < size)
 	{
 		str[i] = c;
 		i++;
 	}
+	str[i] = '\0';
 	return (str);
 }
