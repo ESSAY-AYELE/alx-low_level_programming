703c7f34f76d066154c4381a7b4302586e710f18
ESSAY-AYELE <misganaa352@gmail.com>
2023-08-07 22:27:14 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
  * create_array -  create a string array
- * @size: size of the array
+ * @size: the size of the array
  * @c: the character to fill it with
  * Return: pointer to the array
  */
 char *create_array(unsigned int size, char c)
 {
-	char *str = (char*)malloc(sizeof(c) * size);
+	char *str = (char*)malloc(sizeof(c) * (size + 1));
 	unsigned int i = 0;
 
+	if (str == '\0')
+		return ('\0');
 	while (i < size)
 	{
 		str[i] = c;
