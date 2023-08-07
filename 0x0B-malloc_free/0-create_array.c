53caf3fc33db7fc07ce5d9a46513de8317ef38c5
ESSAY-AYELE <misganaa352@gmail.com>
2023-08-07 22:33:16 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@ char *create_array(unsigned int size, char c)
 	char *str = malloc(sizeof(c) * size);
 	unsigned int i = 0;
 
-	if (str == NULL)
+	if (str == NULL || size == 0)
 		return (NULL);
 	while (i < size)
 	{
