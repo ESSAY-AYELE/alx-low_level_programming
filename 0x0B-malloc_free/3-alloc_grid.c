590f31021298f2e608ebf7a9a46e783675e5506a
ESSAY-AYELE <misganaa352@gmail.com>
2023-08-08 22:33:43 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,28 +1,32 @@
 #include "main.h"
 /**
- * _alloc_grid - create a grid
+ * alloc_grid - create a grid
  * @width: width of the grid
  * @height: height of the grid
  * Return: pointer to a grid
  */
 int **alloc_grid(int width, int height)
 {
-	int i, j;
-	int **arr;
+    int **grid;
+    int i;
 
-	if (width < 1 || height < 1)
-		return (NULL);
-	arr = (int **)malloc(height * sizeof(int));
-	if (arr == NULL)
-		return (NULL);
-	for (i = 0; i < height; i++)
-		arr[i] = (int *)malloc(width * sizeof(int));
-	if (arr[0] == NULL)
-		return (NULL);
-	for (i = 0; i < height; i++)
-	{
-		for (j = 0; j < width; j++)
-			arr[i][j] = 0;
-	}
-	return (arr);
+    if (width <= 0 || height <= 0) 
+        return NULL;
+    grid = malloc(height * sizeof(int *));
+    if (grid == NULL) 
+        return NULL;
+    for (i = 0; i < height; i++) 
+    {
+        grid[i] = cal  loc(width, sizeof(int));
+        if (grid[i] == NULL) {
+            while (i >= 0) {
+                free(grid[i]);
+                i--;
+            }
+            free(grid);
+            return NULL;
+        }
+    }
+
+    return grid;
 }
