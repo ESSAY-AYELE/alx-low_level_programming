0953f47601dec8bdaa520c8f6f6e42056998fb8f
ESSAY-AYELE <misganaa352@gmial.com>
2023-08-08 10:15:04 -0700

    g

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _alloc_grid - create a grid
+ * @width: width of the grid
+ * @height: height of the grid
+ * Return: pointer to a grid
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **arr;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+	arr = malloc(height * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+		arr[i] = malloc(width * sizeof(int));
+	if (arr[0] == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+	}
+	return (arr);
+}
