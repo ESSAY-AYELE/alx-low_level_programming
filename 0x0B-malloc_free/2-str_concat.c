2582a3b817faadd7ad4ccf2f3c0271fee8285f5d
ESSAY-AYELE <misganaa352@gmail.com>
2023-08-08 23:10:12 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,20 +10,16 @@ char *str_concat(char *s1, char *s2)
 	int len = 0, i = 0, j = 0;
 	char *s;
 
-	if (!(s1 && s2))
-		return ("");
-	if (s1 != NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1 && s1[len])
+		len++;
+	while (s2 && s2[i])
 	{
-		while (s1 && s1[len])
-			len++;
-	}
-	if (s2 != NULL)
-	{
-		while (s2 && s2[i])
-		{
-			len++;
-			i++;
-		}
+		len++;
+		i++;
 	}
 	i = 0;
 	s = malloc((len + 1) * sizeof(char));
@@ -31,7 +27,7 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	if (s1 != NULL)
 	{
-		while (s1 && s1[i])
+		while (s1[i])
 		{
 			s[j] = s1[i];
 			j++;
@@ -39,14 +35,11 @@ char *str_concat(char *s1, char *s2)
 		}
 	}
 	i = 0;
-	if (s2 != NULL)
+	while (s2[i])
 	{
-		while (s2 && s2[i])
-		{
-			s[j] = s2[i];
-			i++;
-			j++;
-		}
+		s[j] = s2[i];
+		i++;
+		j++;
 	}
 	s[j] = '\0';
 	return (s);
