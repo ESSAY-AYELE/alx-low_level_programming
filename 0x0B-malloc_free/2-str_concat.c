6494b496852ce6ef7ca565d50a5f3da53f450217
ESSAY-AYELE <misganaa352@gmial.com>
2023-08-08 02:26:48 -0700

    w

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * str_concat - concatniat 2 string
+ * @s1: the first string
+ * @s2: the second string
+ * Return: pointer to the new string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int len = 0, i = 0, j =0;
+	char *s;
+
+	while (s1[len])
+		len++;
+	while (s2[i])
+	{
+		len++;
+		i++;
+	}
+	i = 0;
+	s = malloc ((len + 1) * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		s[j] = s1[i];
+		j++;
+		i++;
+	}
+	i = 0;
+	while (s2[i])
+	{
+		s[j] = s2[i];
+		i++;
+		j++;
+	}
+	s[j] = '\0';
+	return (s);
+}
