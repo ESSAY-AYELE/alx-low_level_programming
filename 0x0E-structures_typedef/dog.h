52c300b6d204be9957e7edaf0d1fc42ce3a9f558
ESSAY-AYELE <misganaa352@gmial.com>
2023-08-14 16:11:56 -0700

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef HEADER
+#define HEADER
+/**
+ * dog - dog struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
