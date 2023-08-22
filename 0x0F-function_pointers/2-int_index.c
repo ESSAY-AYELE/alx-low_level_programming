8929ee45f05e99095a42db2d3ce29b0c6b301043
ESSAY-AYELE <misganaa352@gmial.com>
2023-08-22 13:37:44 -0700

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - it searches int from array with by cmp function
+ * @array: the array to search from
+ * @size: the size of the array
+ * @cmp: pointer to cmp function
+ * Return: the index of first int that satisify the cmp condition
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if((*cmp)(array[i]))
+				return (i);
+		}
+	}
+	return (-1);
+}
