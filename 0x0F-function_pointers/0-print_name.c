af390b8a726db4c729f610714233a6475cc7bde0
ESSAY-AYELE <misganaa352@gmial.com>
2023-08-22 12:56:27 -0700

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - this is just dum
+ * @name: the name to be printed
+ * @f: the function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
