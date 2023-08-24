938685020d8ae8c54a2bf6a0384163ae7f4c8a27
ESSAY-AYELE <misganaa352@gmial.com>
2023-08-24 12:47:17 -0700

    4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_list - free singly linked list
+ * @head: pointer to the head of list
+ */
+void free_list(list_t *head)
+{
+	list_t *temp; 
+
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
