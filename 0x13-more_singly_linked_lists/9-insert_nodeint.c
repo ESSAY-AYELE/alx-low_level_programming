46cc21903cf87df3eb54db4e8df1276c2aa9bc50
ESSAY-AYELE <misganaa352@gmail.com>
2023-08-29 20:54:29 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,15 +14,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (new == NULL)
 		return (NULL);
+	if (idx == 0)
+	{
+		new->n = n;
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	while (i < idx - 1 && node != NULL)
 	{
 		node = node->next;
 		i++;
 	}
 	if (node == NULL)
-	{
 		return (NULL);
-	}
 	new->n = n;
 	new->next = node->next;
 	node->next = new;
