c3234da792e60998ae5282421d5027e9771c1204
ESSAY-AYELE <misganaa352@gmail.com>
2023-08-28 22:04:10 -0500

    5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free the listint_t
+ * @head: pointer to the pointer of the head of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = *head;
+	listint_t *t;
+
+	while (temp != NULL)
+	{
+		t = temp->next;
+		free(temp);
+		temp = t;
+	}
+	free(head);
+}
