7a54a5f2643c05592490d92090b39242e90f8d41
ESSAY-AYELE <misganaa352@gmail.com>
2023-08-30 07:05:10 -0500

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,13 +1,13 @@
 #include "dog.h"
 
 /**
- * print dog - This function takes a pointer to a 
+ * print_dog - This function takes a pointer to a
  * struct dog variable d, and prints its members using printf
  * @d: pointer to struct dog
  */
 void print_dog(struct dog *d)
 {
-	if (!d)
+	if (d == NULL)
 		return;
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
 	printf("Age: %.1f\n", d->age);
