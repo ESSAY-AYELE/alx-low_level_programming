40de0b4fc3a225768877204c2b73ca03a34a3eb3
ESSAY-AYELE <misganaa352@gmail.com>
2023-08-30 07:24:39 -0500

    4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,6 +13,8 @@ typedef struct dog
 	char *owner;
 } dog_t;
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
