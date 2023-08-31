e3975e3e26dd61e17ca3bdb5ea3442b727682246
ESSAY-AYELE <misganaa352@gmail.com>
2023-08-31 07:05:18 -0500

    5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - it computes the number of bit need to be change
+ * in oreder to go from first number to the second number
+ * @n: the first number
+ * @m: the second number
+ * Return: the number of bit need to be changed
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int result = 0, k;
+
+	k = n ^ m;
+	while (k)
+	{
+		if (k & 1)
+			result += 1;
+		k >>= 1;
+	}
+	return (result);
+}
