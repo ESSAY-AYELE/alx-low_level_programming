99399d21fcb5cfb3a0c36fca35e32d611db58eee
ESSAY-AYELE <misganaa352@gmail.com>
2023-09-04 15:19:30 -0500

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_file - create file and write on it 
+ * @filename: the path of the file
+ * @text_content: the content to be writen
+ * Return: 1 on success, -1 on failure 
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, numL;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (fd < 0)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	for (numL = 0; text_content[numL]; numL++)
+		;
+	if (write(fd, text_content, numL++) < 0)
+		return (-1);
+	close(fd);
+	return (1);
+}
