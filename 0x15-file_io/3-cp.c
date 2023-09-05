9fa6671405e389d2ec272215ea1dec9000ca058d
ESSAY-AYELE <misganaa352@gmail.com>
2023-09-05 15:50:15 -0500

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,12 +1,32 @@
 #include "main.h"
 
 /**
+ * err_hand - check type of error and print on stderr
+ * @file_to: flage for the write
+ * @file_from: flage for the read
+ * @av: argument
+ */
+void err_hand(int file_to, int file_from, char **av)
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+}
+
+/**
  * main - copy one file to another file
  * @ac: number of input argument
  * @av: arugments
  * Return: 0 on succes
  */
-int main(int ac, char **av)
+int main(int ac, char *av[])
 {
 	int file_from, file_to, num_rd = 1024, num_wr;
 	char buf[1024];
@@ -18,30 +38,13 @@ int main(int ac, char **av)
 	}
 	file_from = open(av[1], O_RDONLY);
 	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
+	err_hand(file_to, file_from, av);
 	while (num_rd == 1024)
 	{
 		num_rd = read(file_from, buf, 1024);
-		if (num_rd == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-			exit(98);
-		}
+		err_hand(num_wr, num_rd, av);
 		num_wr = write(file_to, buf, 1024);
-		if (num_wr == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			exit(99);
-		}
+		err_hand(num_wr, num_rd, av);
 	}
 	if (close(file_to) == -1)
 	{
