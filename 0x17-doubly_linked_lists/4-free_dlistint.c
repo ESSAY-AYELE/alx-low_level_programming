2c2cc9e5ab868bd02668e651b41d2df4acee269c
ESSAY-AYELE <misganaa352@gmail.com>
2023-10-12 04:29:53 -0500

    4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free the memory location occopied by the list
+ * @head: the head of the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint *tmp_1 = head, *tmp_2;
+
+	while (tmp_1)
+	{
+		tmp_2 = tmp_1->next;
+		free(tmp_1);
+		tmp_1 = tmp2;
+	}
+}
