f07aa695e4fa617e7872e4c0968e5c99ed3bf161
ESSAY-AYELE <misganaa352@gmail.com>
2023-10-12 14:12:54 -0500

    7.5

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,48 +1,54 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - insert node at a given index
- * @h: head of list
- * @idx: the index
- * @n: the data
- * Return: he address of the new node, or NULL if it failed
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
+ *
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp = *h, *new = NULL;
-	unsigned int num = 0;
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
+	new = NULL;
 	if (idx == 0)
-	{
-		new = add_dnodeint(h,n);
-		return (new);
-	}
+		new = add_dnodeint(h, n);
 	else
 	{
-		if (tmp != NULL)
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
 		{
-			while (tmp->prev != NULL)
-				tmp = tmp->prev;
-		}
-		while (tmp != NULL)
-		{
-			if (idx == num)
+			if (i == idx)
 			{
-				if (tmp->next == NULL)
+				if (head->next == NULL)
 					new = add_dnodeint_end(h, n);
 				else
 				{
 					new = malloc(sizeof(dlistint_t));
-					new->n = n;
-					new->next = tmp;
-					new->prev = tmp->prev;
-					tmp->prev = new;
-					new->prev->next = new;
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
 				}
+				break;
 			}
-			num++;
-			tmp = tmp->next;
+			head = head->next;
+			i++;
 		}
 	}
+
 	return (new);
 }
