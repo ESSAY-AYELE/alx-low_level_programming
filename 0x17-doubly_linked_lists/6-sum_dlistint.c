2e936d4dc13a9682c8da74054115954aa7fc36ff
ESSAY-AYELE <misganaa352@gmail.com>
2023-10-12 05:13:06 -0500

    6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum the data of linked list
+ * @head: the head of the liked list
+ * Return: the sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *tmp =head;
+
+	while (tmp != NULL)
+	{
+		sum += tmp->n;
+		tmp = tmp->next;
+	}
+	return (sum);
+}
