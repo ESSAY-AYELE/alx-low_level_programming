d5c708592b96b295a27a4ba56be365f2539df497
ESSAY-AYELE <misganaa352@gmail.com>
2023-11-03 15:48:04 -0500

    6

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,16 +11,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index = key_index((unsigned char *)key, ht->size);
 	hash_node_t *node = NULL;
 
-	if (index >= ht->size)
-		return (NULL);
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 	node = ht->array[index];
-	while (node)
+	while (node && strcmp(node->key, key) != 0)
 	{
-		if (strcmp(node->key, key) == 0)
-			return (node->value);
 		node = node->next;
 	}
-	return (NULL);
+	return ((node == NULL)? NULL: node->value);
 }
