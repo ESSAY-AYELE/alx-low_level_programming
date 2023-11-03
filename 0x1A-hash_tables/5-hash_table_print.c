15e8bd2738b3240ae87bf48b120bbd1087f69ec2
ESSAY-AYELE <misganaa352@gmail.com>
2023-11-03 14:43:31 -0500

    4

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print the hash table
+ * @ht: the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node = NULL;
+	unsigned long int i;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		if (i == 0)
+			printf("{");
+		while (node != NULL)
+		{
+			printf("'%s': '%s'", node->key, node->value);			
+			node = node->next;
+			if (node)
+				printf(", ");
+		}
+		if (i == ht->size - 1)
+			printf("}\n");
+	}
+}
