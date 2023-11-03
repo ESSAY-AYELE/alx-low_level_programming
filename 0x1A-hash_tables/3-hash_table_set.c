87ca820b2037aecc1f537dc2f58645006434aa6c
ESSAY-AYELE <misganaa352@gmail.com>
2023-11-03 11:03:15 -0500

    4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,9 +9,8 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new = NULL;
+	hash_node_t *new, *node;
 	unsigned long int index;
-	hash_node_t *node;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
@@ -21,36 +20,30 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		new = malloc(sizeof(hash_node_t));
 		if (new == NULL)
 			return (0);
-
 		new->key = strdup(key);
 		new->value = strdup(value);
-
 		if (new == NULL || new->key == NULL || new->value == NULL)
 			return (0);
 		ht->array[index] = new;
-		return (1);
 	}
 	else
 	{
 		node = ht->array[index];
 		while (node != NULL)
 		{
-
 			if (strcmp(node->key, key) == 0)
 			{
 				free(node->value);
 				strcpy(node->value, value);
 				break;
 			}
-			node = node->next; 
+			node = node->next;
 		}
 		if (node == NULL)
 		{
 			new = malloc(sizeof(hash_node_t));
 			new->key = malloc(strlen(key) + 1);
-			if (value != NULL)
-				new->value = malloc(strlen(value) + 1);
-
+			new->value = malloc(strlen(value) + 1);
 			if (new == NULL || new->key == NULL || new->value == NULL)
 				return (0);
 			strcpy(new->key, key);
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get the value from hash table
+ * @ht: pointer for the hash table
+ * @key: key associted with the value
+ * Return: the value else NULL IF NOT found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = key_index((unsigned char *)key, ht->size);
+	hash_node_t *node = ht->array[index];
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
+}
