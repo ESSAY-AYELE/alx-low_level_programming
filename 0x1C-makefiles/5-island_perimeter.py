9e438307404a31b27fa9ad22c969a16e5fa66d3a
ESSAY-AYELE <misganaa352@gmail.com>
2023-12-20 09:05:04 -0600

    5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" returns the perimeter of the island described in grid"""
+
+
+def island_perimeter(grid):
+    """ fkflkaj"""
+    width = len(grid)
+    perimeter = 0;
+    for i in range(width):
+        length = len(grid[i])
+        for j in range(length):
+            up, down, left, right = (1, 1, 1, 1)
+            if i != 0:
+                up = not(grid[i - 1][j])
+            if i + 1 != width:
+                down = not(grid[i + 1][j])
+            if j != 0:
+                left = not(grid[i][j -1])
+            if j + 1 != length:
+                right = not(grid[i][j +1])
+            perimeter += grid[i][j] * (up + down + left + right)
+    return perimeter
